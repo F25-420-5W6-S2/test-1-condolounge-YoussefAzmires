263cbd0c07d1e8011875e9f1fa893a00689f24b5
Azmires, Youssef Sami Saadalla <2382783@johnabbottcollege.net>
2025-11-18 11:32:21 -0500

    Made application runnable, downloaded all the ef core design packages

--- CondoLounge/Program.cs
+++ CondoLounge/Program.cs
@@ -1,9 +1,18 @@
+using CondoLounge.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
